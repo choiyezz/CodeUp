0452bf23e08c3005bc7227f5eca9c3b91c15c72f
choiyezz <choiji056@gmail.com>
2022-01-10 22:53:04 +0900

    print

--- 001.py
+++ 001.py
@@ -1,1 +1,27 @@
+# 기본
 print("Hello")
+
+ # 공백을 포함한 문장 출력
+print("Hello world")
+print("Hello", "world")
+
+# 줄바꿈
+print("Hello")
+print("world")
+
+# 작은 따옴표 출력
+print("'Hello'")
+
+#큰 따옴표 출력
+print('"Hello"')
+
+#특수문자 출력
+print("\"!@#$%^&*()\'")
+
+# 파일경로 출력
+print("C:\\Download\\'hello\\'.py")
+
+print("print(\"Hello\\nWorld\")")
+
+
+
