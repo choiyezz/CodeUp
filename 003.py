b29c89e159aa235c694ecf3bda00be7ee51e9ad6
choiyezz <choiji056@gmail.com>
2022-01-11 20:35:46 +0900

    input3

--- 003.py
+++ 003.py
@@ -28,3 +28,17 @@ print(s[4:6])
 # 시:분:초 입력받아 분만 출력하기
 s = input().split(":")
 print(s[1])
+
+# 문자열에서의 덧셈
+# 여러개를 입력받을때 보통 공백문자를 기준으로 나누어진다.
+
+w1, w2 = input().split()
+print(w1 + w2)
+
+# 정수 덧셈
+a, b = map(int, input().split())
+print(a + b)
+
+# 실수 덧셈
+a, b = map(float, input().split())
+print(a + b)
